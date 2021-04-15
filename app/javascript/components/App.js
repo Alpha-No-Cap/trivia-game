9137ad56db5bf25c3294d3573140200f63309a43
nickdsaunders <>
2021-04-15 14:53:38 -0700

    added show functionality for questions and answers and next and previous quesions buttons

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -8,58 +8,58 @@ import mockquestions from "./mockquestions.js";
 import TriviaShow from "./pages/TriviaShow";
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			categories: mockdata,
-			questions: [],
-		};
-	}
-	componentDidMount() {
-		console.log("yo");
-		this.getQuestions();
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: mockdata
+      //   questions: mockquesions
+    };
+  }
 
-	getQuestions = () => {
-		console.log("hello");
-		fetch("http://opentdb.com/api.php?category=10&difficulty=medium&amount=30")
-			.then((response) => {
-				return response.json();
-			})
-			.then((payload) => {
-				this.setState({ questions: payload });
-				console.log(payload);
-			})
-			.catch((errors) => {
-				console.log("questions errors", errors);
-			});
-	};
+  getQuestions = (gameCategory, gameDifficulty) => {
+    return fetch(
+      `https://opentdb.com/api.php?amount=50&category=${gameCategory}&difficulty=${gameDifficulty}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((payload) => {
+        console.log(payload.results);
+        this.setState({ questions: payload.results });
+      })
+      .catch((errors) => {
+        console.log("questions errors", errors);
+      });
+  };
 
-	render() {
-		console.log("hi");
-		return (
-			<Router>
-				<Switch>
-					<Route exact path='/' component={Home} />
-					<Route
-						path='/triviaindex'
-						render={(props) => (
-							<TriviaIndex categories={this.state.categories} />
-						)}
-					/>
-					<Route
-						path='/triviashow/:id'
-						render={(props) => {
-							console.log(this.state.questions);
-							const id = +props.match.params.id;
+  render() {
+    console.log(this.state.questions);
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route
+            path="/triviaindex"
+            render={(props) => (
+              <TriviaIndex
+                url={this.getQuestions}
+                categories={this.state.categories}
+              />
+            )}
+          />
+          <Route
+            path="/triviashow/:id"
+            render={(props) => {
+              console.log(this.state.questions);
+              const id = +props.match.params.id;
 
-							return <TriviaShow question={this.state.questions.results[id]} />;
-						}}
-					/>
-				</Switch>
-			</Router>
-		);
-	}
+              return <TriviaShow question={this.state.questions[id]} />;
+            }}
+          />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- app/javascript/components/pages/TriviaShow.js
+++ app/javascript/components/pages/TriviaShow.js
@@ -1,10 +1,48 @@
-import React, { Component } from "react";
+import React from "react";
+// import React, { useState } from "react";
+import {
+  useParams,
+  useState,
+  useHistory,
+  Redirect,
+  NavLink
+} from "react-router-dom";
+import { Button } from "reactstrap";
 
-class TriviaShow extends Component {
-	render() {
-		const { question } = this.props;
-		console.log(this.props.question);
-		return <></>;
-	}
-}
+const TriviaShow = (props) => {
+  const { id } = useParams();
+
+  const history = useHistory();
+
+  const nextQuestion = () => {
+    const increment = 1;
+    history.push(`/triviashow/${+id + increment}`);
+  };
+  const previousQuestion = () => {
+    const increment = 1;
+    const route = +id - increment;
+    if (route < 0) {
+      return (route = 0 && alert("can not go back"));
+    }
+    history.push(`/triviashow/${route}`);
+  };
+
+  const { question } = props;
+  console.log(props.question);
+  console.log(id);
+  return (
+    <>
+      <h3>{question.question}</h3>
+      <h3>{question.correct_answer}</h3>
+      <h3>{question.incorrect_answers}</h3>
+
+      <button onClick={previousQuestion}>Previous Question</button>
+      <button onClick={nextQuestion}>Next Question</button>
+
+      <NavLink to="/triviaindex">
+        <Button>Exit Game</Button>
+      </NavLink>
+    </>
+  );
+};
 export default TriviaShow;
