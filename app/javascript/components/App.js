ad75ee0a7a2ad6702b3bbff3340aa873b9ed4e2c
nickdsaunders <>
2021-04-19 09:57:40 -0700

    changed nothing

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -13,8 +13,8 @@ class App extends React.Component {
     super(props);
     this.state = {
       categories: mockdata,
-        questions: [],
-        score: 0
+      questions: [],
+      score: 0
     };
   }
 
@@ -35,8 +35,8 @@ class App extends React.Component {
   };
 
   addScore = (points) => {
-    this.setState({score: this.state.score + points } )
-  }
+    this.setState({ score: this.state.score + points });
+  };
 
   render() {
     const {
@@ -44,12 +44,12 @@ class App extends React.Component {
       current_user,
       new_user_route,
       sign_in_route,
-      sign_out_route,
-    } = this.props
+      sign_out_route
+    } = this.props;
 
     console.log("questions: ", this.state.questions);
-    console.log("logged_in:", logged_in)
-    console.log("current user:", current_user)
+    console.log("logged_in:", logged_in);
+    console.log("current user:", current_user);
     return (
       <Router>
         {/* <Header
@@ -59,37 +59,45 @@ class App extends React.Component {
               sign_up_route={ sign_up_route }
               /> */}
         <Switch>
-        
-          <Route exact path="/" render = {(props) => { 
-              return ( <Home  
-                logged_in={ logged_in }
-                sign_in_route={ sign_in_route }
-                sign_out_route={ sign_out_route }
-                sign_up_route={ new_user_route }
-              />)
-          }}/>
-          { logged_in &&
           <Route
-            path="/triviaindex"
-            render={(props) => (
-              <TriviaIndex
-                url={this.getQuestions}
-                categories={this.state.categories}
-                current_user={ current_user }
-              />
-            )}
-          />}
+            exact
+            path="/"
+            render={(props) => {
+              return (
+                <Home
+                  logged_in={logged_in}
+                  sign_in_route={sign_in_route}
+                  sign_out_route={sign_out_route}
+                  sign_up_route={new_user_route}
+                />
+              );
+            }}
+          />
+          {logged_in && (
+            <Route
+              path="/triviaindex"
+              render={(props) => (
+                <TriviaIndex
+                  url={this.getQuestions}
+                  categories={this.state.categories}
+                  current_user={current_user}
+                />
+              )}
+            />
+          )}
           <Route
             path="/triviashow/:id"
             render={(props) => {
               console.log(this.state.questions);
               const id = +props.match.params.id;
 
-              return <TriviaShow 
-                question={this.state.questions[id]} 
-                score = {this.state.score}
-                addScore = {this.addScore}
-               />;
+              return (
+                <TriviaShow
+                  question={this.state.questions[id]}
+                  score={this.state.score}
+                  addScore={this.addScore}
+                />
+              );
             }}
           />
         </Switch>
