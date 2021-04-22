46c6ed0501e0d1b93dc3648e809361ecd0d32a0a
REV004 <rsier004@gmail.com>
2021-04-22 09:44:20 -0700

    got 25 percent done

--- app/javascript/components/pages/TriviaIndex.js
+++ app/javascript/components/pages/TriviaIndex.js
@@ -1,77 +1,77 @@
 import React, { useState, Component } from "react";
 import { useHistory, NavLink } from "react-router-dom";
 import {
-  ButtonDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  UncontrolledButtonDropdown,
-  Button
+	ButtonDropdown,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem,
+	UncontrolledButtonDropdown,
+	Button,
 } from "reactstrap";
 
 const TriviaIndex = (props) => {
-  const [dropdownOpen, setOpen] = useState(false);
-  const [dropdownCat, setOpenCat] = useState(false);
-  const toggle = () => setOpen(!dropdownOpen);
-  const toggleCat = () => setOpenCat(!dropdownCat);
-  const [difficulty, setDifficulty] = useState();
-  const [category, setCategory] = useState();
-  const history = useHistory();
+	const [dropdownOpen, setOpen] = useState(false);
+	const [dropdownCat, setOpenCat] = useState(false);
+	const toggle = () => setOpen(!dropdownOpen);
+	const toggleCat = () => setOpenCat(!dropdownCat);
+	const [difficulty, setDifficulty] = useState();
+	const [category, setCategory] = useState();
+	const history = useHistory();
 
-  const onSubmit = () => {
-    props.setStateCategoryDifficulty(category,difficulty)
-    props.url(category, difficulty).then(() => {
-      history.push("/triviashow/0");
-    });
-  };
+	const onSubmit = () => {
+		props.setStateCategoryDifficulty(category, difficulty);
+		props.url(category, difficulty).then(() => {
+			history.push("/triviashow/0");
+		});
+	};
 
-  return (
-    <>
-      <div className="index-container">
-        <div className="dropdown-container">
-          <UncontrolledButtonDropdown isOpen={dropdownCat} toggle={toggleCat}>
-            <DropdownToggle caret>Select Category</DropdownToggle>
+	return (
+		<>
+			<div className='index-container'>
+				<div className='dropdown-container'>
+					<UncontrolledButtonDropdown isOpen={dropdownCat} toggle={toggleCat}>
+						<DropdownToggle caret>Select Category</DropdownToggle>
 
-            <DropdownMenu>
-              {props.categories.trivia_categories.map((category) => {
-                const handlSetCategory = () => {
-                  setCategory(category.id);
-                };
+						<DropdownMenu>
+							{props.categories.trivia_categories.map((category) => {
+								const handlSetCategory = () => {
+									setCategory(category.id);
+								};
 
-                return (
-                  <DropdownItem onClick={handlSetCategory} key={category.id}>
-                    {category.name}
-                  </DropdownItem>
-                );
-              })}
-            </DropdownMenu>
-          </UncontrolledButtonDropdown>
+								return (
+									<DropdownItem onClick={handlSetCategory} key={category.id}>
+										{category.name}
+									</DropdownItem>
+								);
+							})}
+						</DropdownMenu>
+					</UncontrolledButtonDropdown>
 
-          <br />
+					<br />
 
-          <UncontrolledButtonDropdown isOpen={dropdownOpen} toggle={toggle}>
-            <DropdownToggle caret>Select Difficulty</DropdownToggle>
+					<UncontrolledButtonDropdown isOpen={dropdownOpen} toggle={toggle}>
+						<DropdownToggle caret>Select Difficulty</DropdownToggle>
 
-            <DropdownMenu>
-              <DropdownItem onClick={() => setDifficulty("easy")}>
-                easy
-              </DropdownItem>
-              <DropdownItem onClick={() => setDifficulty("medium")}>
-                medium
-              </DropdownItem>
-              <DropdownItem onClick={() => setDifficulty("hard")}>
-                hard
-              </DropdownItem>
-            </DropdownMenu>
-          </UncontrolledButtonDropdown>
-          <Button onClick={onSubmit}>submit</Button>
-          <NavLink to="/leaderboard">
-            <Button>Leader Board</Button>
-          </NavLink>
-        </div>
-      </div>
-    </>
-  );
+						<DropdownMenu>
+							<DropdownItem onClick={() => setDifficulty("easy")}>
+								easy
+							</DropdownItem>
+							<DropdownItem onClick={() => setDifficulty("medium")}>
+								medium
+							</DropdownItem>
+							<DropdownItem onClick={() => setDifficulty("hard")}>
+								hard
+							</DropdownItem>
+						</DropdownMenu>
+					</UncontrolledButtonDropdown>
+					<Button onClick={onSubmit}>submit</Button>
+					<NavLink to='/leaderboard'>
+						<Button>Leader Board</Button>
+					</NavLink>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default TriviaIndex;
