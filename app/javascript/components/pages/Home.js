e7cfdeb2071f2a65270989c04bb37935d6cb1df1
nickdsaunders <>
2021-04-23 16:07:27 -0700

    styling

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -16,36 +16,42 @@ class Home extends Component {
     return (
       <>
         <div className="home-container">
-          <h2 className="home-header">Welcome</h2>
-          <h4 className="home-instructions">
-            This is a fun game of trivia that you can play on your own or with
-            your friends! Quench your thirst for knowledge by choosing your
-            category and difficulty to start the game. You get more points for
-            each question by increasing the difficulty at the start of the game.
-            So flex you 🧠 power if you dare. Your final score will be ranked on
-            the leaderboard once you go through all the questions of one
-            category, or loose all 4 of your lives (by answering incorrectly).
-          </h4>
-          {!logged_in && (
-            <a href={sign_in_route}>
-              <Button>Sign In</Button>
-            </a>
-          )}
-          {logged_in && (
-            <a href={sign_out_route}>
-              <Button>Sign Out</Button>
-            </a>
-          )}
-          {!logged_in && (
-            <a href={sign_up_route}>
-              <Button>Sign Up</Button>
-            </a>
-          )}
-          {logged_in && (
-            <a href="/triviaindex">
-              <Button>Play Game</Button>
-            </a>
-          )}
+          <div className="home-content">
+            <h2 className="home-header">Welcome❕</h2>
+            <h4 className="home-instructions">
+              This is a fun game of trivia that you can play on your own or with
+              your friends! Quench your thirst for knowledge by choosing your
+              category and difficulty to start the game. You get more points for
+              each question by increasing the difficulty at the start of the
+              game. So flex you 🧠 power if you dare. Your final score will be
+              ranked on the leaderboard once you go through all the questions of
+              one category, or loose all 4 of your lives (by answering
+              incorrectly).
+            </h4>
+
+            <div className="home-buttons">
+              {!logged_in && (
+                <a href={sign_in_route}>
+                  <Button>Sign In</Button>
+                </a>
+              )}
+              {logged_in && (
+                <a href={sign_out_route}>
+                  <Button>Sign Out</Button>
+                </a>
+              )}
+              {!logged_in && (
+                <a href={sign_up_route}>
+                  <Button>Sign Up</Button>
+                </a>
+              )}
+              {logged_in && (
+                <a href="/triviaindex">
+                  <Button>Play Game</Button>
+                </a>
+              )}
+            </div>
+          </div>
         </div>
       </>
     );
