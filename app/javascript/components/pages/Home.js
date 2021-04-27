924e157a114179c0f05eccc18506d05daef4827e
nickdsaunders <>
2021-04-27 09:51:39 -0700

    added about us page

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -20,13 +20,16 @@ class Home extends Component {
             <h2 className="home-header">Welcome❕</h2>
             <h4 className="home-instructions">
               This is a fun game of trivia that you can play on your own or with
-              your friends! Quench your thirst for knowledge by choosing your
-              category and difficulty to start the game. You get more points for
-              each question by increasing the difficulty at the start of the
-              game. So flex you 🧠 power if you dare. Your final score will be
-              ranked on the leaderboard once you go through all the questions of
-              one category, or loose all 4 of your lives (by answering
-              incorrectly).
+              your{" "}
+              <a href="/about" className="about-link">
+                friends!
+              </a>{" "}
+              Quench your thirst for knowledge by choosing your category and
+              difficulty to start the game. You get more points for each
+              question by increasing the difficulty at the start of the game. So
+              flex your 🧠 power if you dare. Your final score will be ranked on
+              the leaderboard once you go through all the questions of one
+              category, or loose all 4 of your lives (by answering incorrectly).
             </h4>
 
             <div className="home-buttons">
